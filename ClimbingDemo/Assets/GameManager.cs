c1c2278991a80471453d0f82aaadf165099bfa85
achamout98 <arc04@mail.aub.edu>
2020-03-10 20:53:57 +0200

    somethign

---
+++ ClimbingDemo/Assets/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject SpawnPoints;
+
+    [SerializeField]
+    private List<GameObject> prefabs;
+
+    private List<GameObject> children;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //int num = SpawnPoints.transform.childCount;
+
+        //for(int i = 0; i < num; i++) {
+        //    children[i] = SpawnPoints.transform.GetChild(i).gameObject;
+        //}
+        StartCoroutine("SpawnRocks");
+    }
+
+    private IEnumerator SpawnRocks () {
+        //foreach(GameObject child in children) {
+        //    Vector3 pos = child.transform.position;
+        //    int index = Random.Range(0, prefabs.Count-1);
+        //    Instantiate(prefabs[index], pos, Quaternion.identity);
+            
+        //    yield return null;
+        //}
+
+        for (int i = 0; i < SpawnPoints.transform.childCount; i++) {
+            Vector3 pos = SpawnPoints.transform.GetChild(i).position;
+
+            int index = Random.Range(0, prefabs.Count - 1);
+            Instantiate(prefabs[index], pos, Quaternion.identity);
+
+            yield return null;
+        }
+        yield break;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ClimbingDemo/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float grabDistance;
+    [SerializeField] private GameObject Child;
+
+    private Rigidbody rb;
+    private Ray ray;
+    private RaycastHit hit;
+
+    private bool mouseclicked;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mouseclicked = false;
+    }
+
+    void Update()
+    {
+        //Player rotation (camera and playerbody both rotate)
+        transform.rotation = Quaternion.LookRotation(Child.transform.forward);
+        //Grabbing rocks
+        Grab();
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    /************************************************************************************************************/
+
+    private void Grab()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            String tag = hit.collider.tag;
+            if (tag.Equals("Rock"))
+            {
+                Transform sphere = hit.collider.gameObject.transform;
+                if (Vector3.Distance(transform.position, sphere.position) <= grabDistance)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        mouseclicked = true;
+
+                        float x = sphere.position.x;
+                        float y = sphere.position.y;
+                        float z = sphere.position.z - 2f;
+
+                        Vector3 new_pos = new Vector3(x, y, z);
+                        StartCoroutine(Climb(new_pos, 0.60f));
+                    }
+                }
+            }
+        }
+    }
+
+    private IEnumerator Climb(Vector3 new_pos, float time) {
+        StopCoroutine("Stay");
+        Time.timeScale = 1.0f;
+        Debug.Log(Time.timeScale);
+        float eta = 0f;
+        Vector3 start_pos = transform.position;
+
+        while (mouseclicked) {
+            while(eta < time) {
+                transform.position = Vector3.Lerp(start_pos, new_pos, (eta / time));
+                eta += Time.deltaTime;
+
+                if (Input.GetMouseButtonUp(0)) {
+                    mouseclicked = false;
+                    StopCoroutine("Climb");
+                    yield break;
+                }
+
+                yield return null;
+            }
+            StartCoroutine(Stay(new_pos));
+            yield return null;
+        }
+        yield return null;
+    }
+
+    private IEnumerator Stay (Vector3 new_pos) {
+        while (mouseclicked) {
+            transform.position = new_pos;
+            if (Input.GetMouseButtonUp(0)) {
+                mouseclicked = false;
+
+                float t = 0f;
+                float duration = 0.2f;
+
+                Time.timeScale = 0.2f;
+                while(t < duration) {
+
+                    t += Time.deltaTime;
+                    Debug.Log(t);
+
+                    yield return null;
+
+                }
+                Time.timeScale = 1.0f;
+
+                yield break;
+            }
+            yield return null;
+        }
+        yield break;
+    }
+
+
+    private void Move()
+    {
+        float hAxis = Input.GetAxisRaw("Horizontal");
+        float vAxis = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * speed * Time.fixedDeltaTime;
+
+        Vector3 newPosition = rb.position + rb.transform.TransformDirection(movement);
+
+        rb.MovePosition(newPosition);
+    }
+}
