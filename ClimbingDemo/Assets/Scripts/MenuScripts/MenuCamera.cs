527e2f7f3d0f2d14e809b6a29da413ec0b25ce52
achamout98 <arc04@mail.aub.edu>
2020-05-10 21:21:53 +0300

    Rotating camera in main menu
    Added a rotation camera to the main menu for extra aesthetic effects

---
+++ ClimbingDemo/Assets/Scripts/MenuScripts/MenuCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamera : MonoBehaviour
+{
+
+    [SerializeField] private float rotationSpeed = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
